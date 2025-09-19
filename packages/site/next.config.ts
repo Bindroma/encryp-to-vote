ed953d38c630f2cd015a4f05224992e2c6205df7
Bindroma <qjgg804164@hotmail.com>
2025-09-19 02:44:45 +0000

    Disable ESLint

--- packages/site/next.config.ts
+++ packages/site/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   headers() {
     // Required by FHEVM 
     return Promise.resolve([
